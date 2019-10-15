f624920663ec6ab64b8ae1393b136f779c63f3ac
Anshuman Singh <anshuman_singh2@apple.com>
2019-10-15 15:33:09 +0530

    Initial Commit

---
+++ StackApplication/Stack.swift
@@ -0,0 +1,36 @@
+//
+//  Stack.swift
+//  StackApplication
+//
+//  Created by anshuman singh on 15/10/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import Foundation
+
+protocol Stack {
+    
+    associatedtype ElementType
+    
+    var peek: ElementType { get }
+    mutating func push(element: ElementType)
+    mutating func pop() -> ElementType?
+}
+
+struct StringStack: Stack {
+    
+    var peek: String
+    private var stackArr = Array<String>()
+    
+    private func isEmpty() -> Bool {
+        return stackArr.count > 0 ? true:false
+    }
+
+    mutating func push(element: String) {
+        stackArr.append(element)
+    }
+
+    mutating func pop() -> String? {
+        return stackArr.popLast()
+    }
+}
